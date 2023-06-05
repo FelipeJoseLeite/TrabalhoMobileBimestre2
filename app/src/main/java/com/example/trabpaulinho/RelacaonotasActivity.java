c0e19431dc7e5e9bd5ffc7980948b56e6d254b89
Mateus-Pessini <mateus.scherer@edu.unipar.br>
2023-06-05 16:15:07 -0300

    Re-colocação dos arquivos

---
+++ app/src/main/java/com/example/trabpaulinho/RelacaonotasActivity.java
@@ -0,0 +1,14 @@
+package com.example.trabpaulinho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelacaonotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relacaonotas);
+    }
+}
